0831cc4852daf1144aa717d8364f07257ef22f84
jpgfigueiredo <johnpeterfigueiredo@gmail.com>
2023-01-18 23:32:03 -0300

    Obtendo chave valor

--- src/Aula0046-generics-4/Aula0046-generics-4.ts
+++ src/Aula0046-generics-4/Aula0046-generics-4.ts
@@ -2,3 +2,14 @@
 type ObterChaveFn = <O, K extends keyof O>(objeto: O, chave: K) => O[K];
 
 const obterChave: ObterChaveFn = (objeto, chave) => objeto[chave];
+
+const animal = {
+  cor: 'rosa',
+  vacinas: ['Vacina 1', 'VCacina 2'],
+  idade: 9,
+};
+
+const vacinas = obterChave(animal, 'vacinas');
+const cor = obterChave(animal, 'cor');
+
+console.log(vacinas, cor);
